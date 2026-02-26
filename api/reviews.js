f98f8da5a799cbbe040b9c4553f7bcb0b351895f
bao-vo-90 <bao@prosperdigital.com.au>
2026-02-26 15:11:59 +0700

    Add pushlish param

--- api/reviews.js
+++ api/reviews.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const url = `https://api.judge.me/api/v1/reviews?api_token=${API_TOKEN_EU}&shop_domain=${SHOP_DOMAIN_EU}&per_page=10&rating=5${product_id ? `&product_id=${product_id}` : ''}`;
+    const url = `https://api.judge.me/api/v1/reviews?api_token=${API_TOKEN_EU}&shop_domain=${SHOP_DOMAIN_EU}&per_page=10&rating=5&published=true${product_id ? `&product_id=${product_id}` : ''}`;
 
     const response = await fetch(url);
     res.setHeader('Access-Control-Allow-Origin', '*');
